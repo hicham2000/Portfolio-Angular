907dc3f78a6e722f47685dca4c30392a1499295b
hicham <oo906496@gmail.com>
2023-02-07 19:56:07 +0100

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProfilComponent } from './profil/profil.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CertificateComponent } from './certificate/certificate.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SkillsComponent,
+    ProfilComponent,
+    PortfolioComponent,
+    CertificateComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
