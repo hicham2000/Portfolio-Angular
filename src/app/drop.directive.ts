20152eceb6864435999abe9ea38dabff0e465238
hicham <oo906496@gmail.com>
2023-02-16 21:18:06 +0100

    changes im the drop down

--- src/app/drop.directive.ts
+++ src/app/drop.directive.ts
@@ -1,16 +1,47 @@
-import {Directive, HostBinding, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appDrop]'
 })
 export class DropDirective {
 
-  @HostBinding("class.open") isopen=false;
+  @Input() header!:HTMLDivElement;
 
-  @HostListener('document:click',['$event']) onclick(event:Event){
+  open:boolean=false;
+  constructor(private  element:ElementRef,private renderer: Renderer2) { }
+
+  removeclass(){
+    this.renderer.addClass(this.element.nativeElement,'collapsed');
+    this.renderer.setAttribute(this.element.nativeElement, 'aria-expanded','false');
+
+    this.header.setAttribute('class','collapse navbar-collapse');
+    this.open=!this.open;
+  }
+  @HostListener('click') onclick(){
     // this.isopen=this.ele.nat
+
+    if(!this.open){
+
+      this.renderer.removeClass(this.element.nativeElement,'collapsed');
+      this.renderer.setAttribute(this.element.nativeElement, 'aria-expanded','true');
+
+      this.header.setAttribute('class','show collapse navbar-collapse');
+      this.open=!this.open;
+
+    }
+
+    else{
+      this.removeclass();
+
+    }
   }
 
-  constructor() { }
+  @HostListener('document:click', ['$event.target'])
+  onDocumentClick(targetElement:any) {
+    const clickedInside = this.element.nativeElement.contains(targetElement);
+    if (!clickedInside) {
+      this.removeclass();
+    }
+  }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {faEarthAfrica} from '@fortawesome/free-solid-svg-icons';
 import {DropdownService} from "../dropdown.service";
+import {state, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+
 })
 export class HeaderComponent {
   faEarthAfrica = faEarthAfrica;
 
+  @ViewChild('header') header!:ElementRef;
+
   valid:boolean=true;
   onclick(){
-    let div= document.getElementById('header');
 
-   // @ts-ignore
-    this.valid ? div.classList.add("show"):div.classList.remove("show");
-    this.valid = !this.valid;
+
+
+
+   //  let div= document.getElementById('header');
+   //
+   // // @ts-ignore
+   //  this.valid ? div.classList.add("show"):div.classList.remove("show");
+   //  this.valid = !this.valid;
   }
 
   constructor(private dropdown: DropdownService) {
@@ -29,4 +37,8 @@ export class HeaderComponent {
     this.valid = !this.valid;
 
   }
+
+  show:string='show';
+
+
 }
